c35fa52f8b376f26755ca0ff9d0813aeb5a0bc57
Windows <sjc123580@gmail.com>
2024-03-01 15:03:27 +0800

    init

---
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -0,0 +1,29 @@
+package com.sky.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class EmployeeDTO implements Serializable {
+
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String phone;
+
+    private String sex;
+
+    private String idNumber;
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    public void save(EmployeeDTO employeeDTO) {
+
+    }
+}
